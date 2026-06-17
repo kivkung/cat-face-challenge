f2628b6ea9178fa8bfc4160df324469cbf01be58
kongki <kivkung.2@gmail.com>
2026-06-17 20:27:00 +0700

    i forgot the line

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import { GoogleGenAI } from '@google/genai';
 
-app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:3000', 'https://majestic-lily-d27a3f.netlify.app/']
-}));
-
 const app = express();
 // ขยายขนาดการรับข้อมูลให้เหมาะสม
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
+
+app.use(cors({
+  origin: ['http://127.0.0.1:5500', 'http://localhost:3000', 'https://majestic-lily-d27a3f.netlify.app/']
+}));
 
 if (!process.env.GEMINI_API_KEY) {
   console.error("❌ กรุณาตั้งค่า GEMINI_API_KEY ในไฟล์ .env");
